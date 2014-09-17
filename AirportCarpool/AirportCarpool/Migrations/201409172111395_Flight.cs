e9730e08d0abaacf610d32edac6708af118d9d40
Tom Celis <tce@organi.be>
2014-09-17 23:13:45 +0200

    naam verandering undo

---
+++ AirportCarpool/AirportCarpool/Migrations/201409172111395_Flight.cs
@@ -0,0 +1,16 @@
+namespace AirportCarpool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Flight : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
