5564d17592493678059202500028c22b3b36d658
Glenn Van den Hende <gvh@organi.be>
2014-09-19 15:50:51 +0200

    gvh - project + global

--- AirportCarpool/AirportCarpool/Global.asax.cs
+++ AirportCarpool/AirportCarpool/Global.asax.cs
@@ -7,19 +7,16 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace AirportCarpool
-{
+namespace AirportCarpool {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
+    public class MvcApplication : System.Web.HttpApplication {
+        protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
 
-           WebApiConfig.Register(GlobalConfiguration.Configuration);
-           // GlobalConfiguration.Configure(WebApiConfig.Register);
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
