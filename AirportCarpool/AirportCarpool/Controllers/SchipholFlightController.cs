69f3f9edaf031f91b42e5d7796a1a1966260bd9d
Tom Celis <tce@organi.be>
2014-09-20 15:19:53 +0200

    SchipholFlights toegevoegd

--- AirportCarpool/AirportCarpool/Controllers/SchipholFlightController.cs
+++ AirportCarpool/AirportCarpool/Controllers/SchipholFlightController.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Mvc;
+using AirportCarpool.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AirportCarpool.Controllers
 {
-    public class SchipholFlightController : ApiController
-    {
-        // GET api/schipholflight
-        public IEnumerable<string> Get()
+    public class SchipholFlightController : Controller
+    {        
+        public ActionResult Index()
         {
-            return new string[] { "value1", "value2" };
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = client.GetAsync("http://145.35.195.100/rest/flights").Result;
+          
+            
+            var resultString = response.Content.ReadAsStringAsync().Result;
+            List<SchipholFlight> flights = new List<SchipholFlight>();
+         
+            JObject flights_ = JObject.Parse(resultString);
+
+            // get JSON result objects into a list
+            IList<JToken> results = flights_["Flights"]["Flight"].Children().ToList();
+
+            foreach (JToken result in results)
+            {
+                SchipholFlight flight = JsonConvert.DeserializeObject<SchipholFlight>(result.ToString());
+                flights.Add(flight);
+            }
+
+             //(List<SchipholFlight>)Newtonsoft.Json.JsonConvert.DeserializeObject(Request["jsonString"], typeof(List<test>));
+            return View(flights);
         }
 
         // GET api/schipholflight/5
