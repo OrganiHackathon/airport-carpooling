d71aeefb1eb8c9d5f8d8d9e15651ea8c5bbe425b
Glenn Van den Hende <gvh@organi.be>
2014-09-20 19:13:28 +0200

    gvh - NewMovement: view

--- AirportCarpool/AirportCarpool/Controllers/CarpoolController.cs
+++ AirportCarpool/AirportCarpool/Controllers/CarpoolController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AirportCarpool.Models;
 
 namespace AirportCarpool.Controllers {
     public class CarpoolController : Controller {
@@ -13,13 +14,33 @@ namespace AirportCarpool.Controllers {
         }
 
         public ActionResult NewMovement() {
-            ViewBag.LocationFrom = new SelectList((new string[] {"Home", "Schiphol", "Other address"}).ToList());
-            ViewBag.LocationTo = ViewBag.LocationFrom;
-
-            return View();
+            NewMovementFillViewBag();
+            return View(new NewMovement());
         }
-        public ActionResult NewMovement2() {
-            return View();
+
+        [HttpPost]
+        public ActionResult NewMovement(NewMovement newMovement) {
+            var locationFrom = newMovement.LocationFrom;
+            var locationTo = newMovement.LocationTo;
+
+            var valid = ModelState.IsValid;
+
+            if (locationFrom == locationTo && locationFrom != "Other address" && locationTo != "Other address") {
+                ModelState.AddModelError("LocationTo", string.Format("The location to ({0}) must be different from the location from ({1}).", locationTo, locationFrom));
+                valid = false;
+            }
+            if (valid) {
+                return View("");
+            }
+            
+            // Invalid:
+            NewMovementFillViewBag();
+            return View(newMovement);
+        }
+
+        private void NewMovementFillViewBag() {
+            ViewBag.LocationFrom = new SelectList((new string[] { "Home", "Schiphol", "Other address" }).ToList());
+            ViewBag.LocationTo = ViewBag.LocationFrom;
         }
     }
 }
---
+++ AirportCarpool/AirportCarpool/Scripts/Site.js
@@ -0,0 +1,34 @@
+﻿
+$(function () {
+    var effectTime = 300; // ms
+
+    $('#LocationFrom').change(function () {
+        var value = $(this).val();
+        if (value == 'Other address') {
+            $('#OtherAddressFrom').show(effectTime);
+        } else {
+            $('#OtherAddressFrom').hide(effectTime);
+        }
+    });
+    $('#LocationTo').change(function () {
+        var value = $(this).val();
+        if (value == 'Other address') {
+            $('#OtherAddressTo').show(effectTime);
+        } else {
+            $('#OtherAddressTo').hide(effectTime);
+        }
+    });
+    $('#Driver').change(function () {
+        var value = $(this).is(':checked');
+        if (value) {
+            $('#DriverFields').show(effectTime);
+        } else {
+            $('#DriverFields').hide(effectTime);
+        }
+    });
+    if ($('#LocationFrom').length > 0) {
+        $('#LocationFrom').trigger('change');
+        $('#LocationTo').trigger('change');
+        $('#Driver').trigger('change');
+    }
+});
