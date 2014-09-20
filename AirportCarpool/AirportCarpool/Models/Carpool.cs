137f870d67d78f5c98e74028c98e1e519afc3e5d
Tom Celis <tce@organi.be>
2014-09-21 00:19:41 +0200

    DB wijziging

--- AirportCarpool/AirportCarpool/Models/Carpool.cs
+++ AirportCarpool/AirportCarpool/Models/Carpool.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Web;
 
 namespace AirportCarpool.Models {
+
+    public enum CarpoolStatus{
+        Full,       
+        New
+    }
+
     public class Carpool {
         public int CarpoolId { get; set; }
         public int MaxSeats { get; set; }
         public int MaxLuggage { get; set; }
         public int MaxKm { get; set; }
         public DateTime Arrival { get; set; }
+        public CarpoolStatus Status { get; set; }
 
     }
 }
