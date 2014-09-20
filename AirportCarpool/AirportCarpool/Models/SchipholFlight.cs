09367f31bf2ee8b61a53fd0389e383db7002711c
Tom Celis <tce@organi.be>
2014-09-20 13:15:24 +0200

    schipholflight

---
+++ AirportCarpool/AirportCarpool/Models/SchipholFlight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirportCarpool.Models
+{
+    public class SchipholFlight
+    {
+        public int FlightId { get; set; }
+        public string ArrDep { get; set; }
+        public string Prefix { get; set; }
+        public string PrefixIATA { get; set; }
+        public string PRefixICAO { get; set; }
+        public string Suffix { get; set; }
+        public DateTime ScheduleDate { get; set; }
+        public DateTime ScheduleTime { get; set; }
+
+    }
+}
