d18882ac530b06c34135ef8ab6a284c83b7e3e4f
Glenn Van den Hende <gvh@organi.be>
2014-09-21 03:43:47 +0200

    gvh - save movement to db

--- AirportCarpool/AirportCarpool/Controllers/TripController.cs
+++ AirportCarpool/AirportCarpool/Controllers/TripController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AirportCarpool.Models;
 using AirportCarpool.Services;
+using WebMatrix.WebData;
 
 namespace AirportCarpool.Controllers {
     public class TripController : Controller {
@@ -99,8 +101,74 @@ namespace AirportCarpool.Controllers {
             ViewBag.ListFlights = flights;
         }
 
-        private void SaveNewTrip(NewTrip newTrip) {
+        private Movement SaveNewTrip(NewTrip newTrip) {
+            var movement = new Movement();
+            var db = new AirportCarpoolDbContext();
+            db.Entry(movement).State = EntityState.Added;
 
+            var user = GetUserByUserName(db, WebSecurity.CurrentUserName);
+            db.Entry(user).State = EntityState.Unchanged;
+
+            movement.User = user;
+            Location locationFrom = null;
+            if (newTrip.LocationFrom == "Home" && user.Location != null) {
+                locationFrom = Location.GetCopy(user.Location);
+            } else if (newTrip.LocationFrom == "Schiphol") {
+                locationFrom = Location.GetNewSchipholLocation();
+            }
+            if (locationFrom != null) {
+                db.Entry(locationFrom).State = EntityState.Added;
+                //db.SaveChanges();
+            }
+            movement.LocationFrom = locationFrom;
+
+            Location locationTo = null;
+            if (newTrip.LocationTo == "Home" && user.Location != null) {
+                locationTo = Location.GetCopy(user.Location);
+            } else if (newTrip.LocationTo == "Schiphol") {
+                locationTo = Location.GetNewSchipholLocation();
+            }
+            if (locationTo != null) {
+                db.Entry(locationTo).State = EntityState.Added;
+                //db.SaveChanges();
+            }
+            movement.LocationTo = locationTo;
+
+            movement.Driver = newTrip.Driver;
+            movement.Passenger = newTrip.Passenger;
+            movement.Seats = newTrip.Seats;
+            movement.Luggage = newTrip.Luggage;
+            if (newTrip.Driver) {
+                movement.MaxSeats = newTrip.MaxSeats;
+                movement.MaxLuggage = newTrip.MaxLuggage;
+                movement.MaxKm = newTrip.MaxKm;
+            }
+            movement.MovementDateTime = newTrip.MovementDate.Add(newTrip.MovementTime.TimeOfDay);
+            movement.MovementDateType = newTrip.MovementDateType;
+
+            //if (newTrip.FlightNumber != "") {
+            //    Flight flight = new Flight {
+            //        FlightNumber = newTrip.FlightNumber,
+            //        ArrDep = newTrip.ArrDep,
+            //        Arrival = newTrip.Arrival,
+            //        Departure = newTrip.Departure,
+            //        Date = newTrip.MovementDate
+            //    };
+            //    flight.Movement = movement;
+            //    //movement.MovementDetails = new List<MovementDetail>();
+            //    //movement.MovementDetails.Add(flight);
+            //    db.Entry(flight).State = EntityState.Added;
+            //}
+            
+            db.SaveChanges();
+
+            return movement;
+        }
+
+        private User GetUserByUserName(AirportCarpoolDbContext db, string userName) {
+            return (from u in db.Users
+                    where u.UserName == userName
+                    select u).SingleOrDefault<User>();
         }
     }
 }
--- AirportCarpool/AirportCarpool/Models/Location.cs
+++ AirportCarpool/AirportCarpool/Models/Location.cs
@@ -13,5 +13,29 @@ namespace AirportCarpool.Models {
         public string Country { get; set; }
         public string Geolocation { get; set; }
 
+        public Location() {
+
+        }
+
+        public static Location GetCopy(Location location) {
+            return new Location {
+                Street = location.Street,
+                StreetNr = location.StreetNr,
+                PostalCode = location.PostalCode,
+                City = location.City,
+                Country = location.Country,
+                Geolocation = location.Geolocation
+            };
+        }
+
+        public static Location GetNewSchipholLocation() {
+            return new Location {
+                Street = "Evert van der Beekstraat",
+                StreetNr = "202",
+                PostalCode = "1118 CP",
+                City = "Schiphol",
+                Country = "Nederland"
+            };
+        }
     }
 }
