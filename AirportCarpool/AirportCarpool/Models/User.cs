cf9aa3b4c3ca5d3d96d1601892e985fc438c7c6c
Peter De Saegher <pds@organi.be>
2014-09-21 07:11:24 +0200

    User location

--- AirportCarpool/AirportCarpool/Models/User.cs
+++ AirportCarpool/AirportCarpool/Models/User.cs
@@ -18,8 +18,44 @@ namespace AirportCarpool.Models {
         public string SurName { get; set; }
         public string GSM { get; set; }
         public virtual Location Location { get; set; }
+
+        public UserViewModel ViewModel() {
+            UserViewModel vm = new UserViewModel();
+
+            vm.UserId = UserId;
+            vm.UserName = UserName;
+            vm.Email = Email;
+            vm.Name = Name;
+            vm.SurName = SurName;
+            vm.GSM = GSM;
+            vm.LocationId = Location.LocationId;
+            vm.Street = Location.Street;
+            vm.StreetNr = Location.StreetNr;
+            vm.PostalCode = Location.PostalCode;
+            vm.City = Location.City;
+            vm.Country = Location.Country;
+            vm.Geolocation = Location.Geolocation;
+
+            return vm;
+        }
+        public void FillFromViewModel(UserViewModel vm) {
+            UserId = vm.UserId;
+            UserName = vm.UserName;
+            Email = vm.Email;
+            Name = vm.Name;
+            SurName = vm.SurName;
+            GSM = vm.GSM;
+            Location.LocationId = vm.LocationId;
+            Location.Street = vm.Street;
+            Location.StreetNr = vm.StreetNr;
+            Location.PostalCode = vm.PostalCode;
+            Location.City = vm.City;
+            Location.Country = vm.Country;
+            Location.Geolocation = vm.Geolocation;
+        }
     }
 
+    
 
 
 
