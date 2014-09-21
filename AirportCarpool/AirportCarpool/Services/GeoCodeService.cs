ef1a16c23b70c1c66928cd56be6c19e22f6d6ac9
Peter De Saegher <pds@organi.be>
2014-09-21 08:09:14 +0200

    Adres geocoded

--- AirportCarpool/AirportCarpool/Services/GeoCodeService.cs
+++ AirportCarpool/AirportCarpool/Services/GeoCodeService.cs
@@ -17,23 +17,27 @@ namespace AirportCarpool.Services
 
         public string AdressToGeoCode(string street, string streetNr, string postCode, string country){
 
-            //https://api.tomtom.com/lbs/services/geocode/4/geocode?ST=32&T=bosduifstraat&PC=2018&CN=belgium&key=cnjzskcgdmzd25xkq6eudpqc
+            //vb https://api.tomtom.com/lbs/services/geocode/4/geocode?ST=32&T=bosduifstraat&PC=2018&CN=belgium&key=cnjzskcgdmzd25xkq6eudpqc
             string geoCode = string.Empty;
-            string reqUrl = string.Format("({0}?ST={1}&T={2}&PC={3}&CN={4}&key={5}", BaseUrl, HttpUtility.UrlEncode(streetNr), HttpUtility.UrlEncode(street), HttpUtility.UrlEncode(postCode), HttpUtility.UrlEncode(country));
-            HttpClient client = new HttpClient();
-            System.Net.HttpWebRequest request = WebRequest.Create(reqUrl) as HttpWebRequest;
-            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            try
+            {
+                string reqUrl = string.Format("{0}?ST={1}&T={2}&PC={3}&CN={4}&key={5}", BaseUrl, HttpUtility.UrlEncode(streetNr), HttpUtility.UrlEncode(street), HttpUtility.UrlEncode(postCode), HttpUtility.UrlEncode(country), ApiKey);
+                HttpClient client = new HttpClient();
+                System.Net.HttpWebRequest request = WebRequest.Create(reqUrl) as HttpWebRequest;
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
 
 
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(response.GetResponseStream());
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(response.GetResponseStream());
 
-            //*[local-name()="summary"]
-            XmlNodeList nodeList = xmlDoc.SelectNodes("//*[local-name()='geoResult']");
-            if (nodeList.Count > 0) {
-                geoCode = string.Format("{0}:{1}", nodeList.Item(0).SelectSingleNode("latitude").InnerText, nodeList.Item(0).SelectSingleNode("longitude").InnerText);
+                //*[local-name()="summary"]
+                XmlNodeList nodeList = xmlDoc.SelectNodes("//*[local-name()='geoResult']");
+                if (nodeList.Count > 0)
+                {
+                    geoCode = string.Format("{0}:{1}", nodeList.Item(0).SelectSingleNode("*[local-name()='latitude']").InnerText, nodeList.Item(0).SelectSingleNode("*[local-name()='longitude']").InnerText);
+                }
             }
-            
+            catch (Exception) { }
             return geoCode;
         }
     }
