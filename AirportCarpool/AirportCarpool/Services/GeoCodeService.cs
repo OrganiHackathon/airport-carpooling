8b7c57ace5c944b68cdf48c3cde39fc063cee490
Peter De Saegher <pds@organi.be>
2014-09-21 07:33:08 +0200

    GeoCodeService

---
+++ AirportCarpool/AirportCarpool/Services/GeoCodeService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace AirportCarpool.Services
+{
+    public class GeoCodeService
+    {
+        private const string ApiKey = "cnjzskcgdmzd25xkq6eudpqc";
+        private const string BaseUrl = "https://api.tomtom.com/lbs/services/geocode/4/geocode";
+
+        public GeoCodeService() { }
+
+        public string AdressToGeoCode(string street, string streetNr, string postCode, string country){
+
+            //https://api.tomtom.com/lbs/services/geocode/4/geocode?ST=32&T=bosduifstraat&PC=2018&CN=belgium&key=cnjzskcgdmzd25xkq6eudpqc
+            string geoCode = string.Empty;
+            string reqUrl = string.Format("({0}?ST={1}&T={2}&PC={3}&CN={4}&key={5}", BaseUrl, HttpUtility.UrlEncode(streetNr), HttpUtility.UrlEncode(street), HttpUtility.UrlEncode(postCode), HttpUtility.UrlEncode(country));
+            HttpClient client = new HttpClient();
+            
+            return geoCode;
+        }
+    }
+}
